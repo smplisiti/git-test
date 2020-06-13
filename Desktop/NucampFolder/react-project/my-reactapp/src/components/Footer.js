47d82380f51b56912fffc9136a065572f853b639
Heather <smplisiti@gmail.com>
2020-06-12 18:16:43 -1000

    add Footer

--- Desktop/NucampFolder/react-project/my-reactapp/src/components/Footer.js
+++ Desktop/NucampFolder/react-project/my-reactapp/src/components/Footer.js
@@ -2,13 +2,21 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer class="white-section" id="footer">
+       
+     
+       
+       <footer className="white-section" id="footer">
     <div class="bottom-container">
-      <a href="https://www.facebook.com/"><i class="social-icon fab fa-facebook"></i></a>
-      <a href="https://twitter.com/explore"><i class="social-icon fab fa-twitter"></i></a>
-      <a href="https://www.instagram.com/"><i class="social-icon fab fa-instagram"></i></a>
+
+    <a href="https://www.apple.com/ios/app-store/" button class="download-button btn btn-lg btn-dark" type="button"><i className="icon fab fa-apple"></i></a>Download
+      <a href="https://play.google.com/store/apps" button class="download-button btn btn-lg brn-light" style="color:black;" type="button"><i className="icon fab fa-google-play"></i></a>
+        Download
+
+      <a href="https://www.facebook.com/"><i className="social-icon fab fa-facebook"></i></a>
+      <a href="https://twitter.com/explore"><i className="social-icon fab fa-twitter"></i></a>
+      <a href="https://www.instagram.com/"><i className="social-icon fab fa-instagram"></i></a>
       
-      <h6 class="copyright">© Copyright 2020 PlentyOfWoof</h6>
+      <h6 className="copyright">© Copyright 2020 PlentyOfWoof</h6>
     </div>
   </footer>
     )
